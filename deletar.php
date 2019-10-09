b7f676d1f2cae65b506c62351156cc97a7894937
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2019-10-09 19:18:57 -0300

    Create deletar.php

---
+++ deletar.php
@@ -0,0 +1,17 @@
+<?php
+ require 'config.php';
+
+ if(isset($_GET['id']) && !empty($_GET['id'])){
+     $id = addslashes($_GET['id']);
+   $sql = "DELETE FROM usuarios WHERE id =:id ";
+   $sql = $pdo->prepare($sql);
+   $sql->bindValue(':id',$id);
+   $sql->execute();
+
+    header("Location: index.php");
+ }else{
+ 	header("Location: index.php");
+ }
+
+
+?>
